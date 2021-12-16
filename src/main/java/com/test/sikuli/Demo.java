9842042322d49bc8b36d1cd932eeea25c44277b2
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-16 15:58:27 +0530

    Update Demo.java

--- src/main/java/com/test/sikuli/Demo.java
+++ src/main/java/com/test/sikuli/Demo.java
@@ -1,5 +1,20 @@
 package com.test.sikuli;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Demo {
 
+	@Test
+	public void testMethod() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://blueimp.github.io/jQuery-File-Upload/"); //Added a demo website
+		
+	}
+	
+	
 }
