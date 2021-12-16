50d14fb59a2cb2d919921006190e5ffb76e6e826
Prasanga Fernando <prasangafdz@outlook.com>
2021-12-16 21:57:40 +0530

    Updated POM with new SikuliX dependency

--- src/main/java/com/test/sikuli/Demo.java
+++ src/main/java/com/test/sikuli/Demo.java
@@ -2,6 +2,8 @@ package com.test.sikuli;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.sikuli.script.Pattern;
+import org.sikuli.script.Screen;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,6 +15,9 @@ public class Demo {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://blueimp.github.io/jQuery-File-Upload/"); //Added a demo website
+		
+		Screen s = new Screen();
+        Pattern fileInputTextBox = new Pattern("Add filepath here");
 		
 	}
 	
