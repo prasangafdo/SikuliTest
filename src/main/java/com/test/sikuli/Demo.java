255289486209ebfb1c3cbbbfce5b6d2e510e9ad3
Prasanga Fernando <prasangafdz@outlook.com>
2022-02-13 10:53:47 +0530

    Manually added SikulixAPI jar since the Maven dependency didn't ork

--- src/main/java/com/test/sikuli/Demo.java
+++ src/main/java/com/test/sikuli/Demo.java
@@ -1,7 +1,10 @@
 package com.test.sikuli;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.sikuli.script.FindFailed;
 import org.sikuli.script.Pattern;
 import org.sikuli.script.Screen;
 import org.testng.annotations.Test;
@@ -11,15 +14,44 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Demo {
 
 	@Test
-	public void testMethod() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://blueimp.github.io/jQuery-File-Upload/"); //Added a demo website
-		
-		Screen s = new Screen();
-        Pattern fileInputTextBox = new Pattern("Add filepath here");
-		
+	public void testMethod() throws FindFailed, InterruptedException {
+		Screen screen = new Screen();
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://demo.guru99.com/test/image_upload/index.php"); //Added a demo website
+        
+      //  Thread.sleep(5000);
+
+        //	driver.findElement(By.xpath("//input[@id='photoimg']")).click();
+      //  driver.findElement(By.xpath("//input[@type='file']")).click();
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("document.getElementById('photoimg').click()");
+        // driver.findElement(By.xpath(".//*[@id='photoimg']")).click();
+//		String filePath = "C:/Users/Prasanga Fernando/Desktop/";
+//		Screen s = new Screen();
+//        Pattern openButton = new Pattern(filePath+"openButton.PNG");
+//        Pattern filenameField = new Pattern(filePath+"filenameField.PNG");
+//
+//
+//       // s.wait(openButton, 20);
+//        s.type(filenameField,"diaphanous.txt");
+//        s.click(openButton);
+
+        Actions action = new Actions(driver);
+        action.moveToElement(driver.findElement(By.xpath(".//*[@id='photoimg']"))).click().build().perform();
+        Thread.sleep(3000);
+        String filePath = "C:/Users/Prasanga Fernando/Desktop/";
+
+        Pattern openButton = new Pattern(filePath+"openButton.PNG");
+        Pattern filenameField = new Pattern(filePath+"filenameField.PNG");
+
+
+       // s.wait(openButton, 20);
+        screen.type(filenameField,"diaphanous.txt");
+        screen.click(openButton);
+	//	driver.close();
 	}
+	
 	
 	
 }
